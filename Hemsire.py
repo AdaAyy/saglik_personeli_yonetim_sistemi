15b05c31818dfc359cabf9a639e9703e4a5ee180
AdaAyy <161303439+AdaAyy@users.noreply.github.com>
2024-05-30 00:46:19 +0300

    Add files via upload

---
+++ Hemsire.py
@@ -0,0 +1,30 @@
+from Personel import Personel
+
+class Personel:
+    def __init__(self, personel_no, ad, soyad, departman, maas, calisma_saati, sertifika, hastane):
+        super().__init__(personel_no, ad, soyad, departman, maas)
+        self.__calisma_saati = calisma_saati
+        self.__sertifika = sertifika
+        self.__hastane = hastane
+
+    def get_calisma_saati(self):
+        return self.__calisma_saati
+    def set_calisma_saati(self, yeni_deger):
+        self.__calisma_saati = yeni_deger
+
+    def get_sertifika(self):
+        return self.__sertifika
+    def set_sertifika(self, yeni_deger):
+        self.__sertifika = yeni_deger
+
+    def get_hastane(self):
+        return self.__hastane
+    def set_hastane(self, yeni_deger):
+        self.__hastane = yeni_deger
+
+
+    def __str__(self):
+        return f"{super().__str__()}\nÇalışma Saati:{self.__calisma_saati}\nSertifika:{self.__sertifika}\nHastane Adı:{self.__hastane}"
+
+    def maas_arttir(self, yuzde):
+        self.set_maas(self.get_maas()*(1 + yuzde/100))
