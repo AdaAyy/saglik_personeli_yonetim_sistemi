1d486f280f9b52ae95b7a105fc884709315be3fb
AdaAyy <161303439+AdaAyy@users.noreply.github.com>
2024-05-30 01:19:05 +0300

    Update Hemsire.py

--- Hemsire.py
+++ Hemsire.py
@@ -1,6 +1,6 @@
 from Personel import Personel
 
-class Personel:
+class Hemsire(Personel):
     def __init__(self, personel_no, ad, soyad, departman, maas, calisma_saati, sertifika, hastane):
         super().__init__(personel_no, ad, soyad, departman, maas)
         self.__calisma_saati = calisma_saati
